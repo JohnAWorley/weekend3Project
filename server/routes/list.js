c7a3f776accbd842fb96dc5e470904bc6bb8ee18
John Worley <john.aver.worley@gmail.com>
2018-11-23 18:02:59 -0600

    more complex project setup

---
+++ server/routes/list.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const listRouter = express.Router();
+const pg = require('pg');
+
+
+
+module.exports = listRouter;
---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 5000;
+const toDo= require('./routes/list.js')
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+
+app.use('/list', toDo);
+
+app.listen(PORT, () => {
+    console.log('listening on port', PORT);
+    
+})
